ffee01f1c51ee201e3d4e2c3de392dee3bbf4586
Quyen Ho <quyenhooppa@Quyens-MBP.net.fpt>
2019-09-21 09:01:02 +0700

    create Lab3

---
+++ Lab2/DigitalWatch.X/main.c
@@ -0,0 +1,13 @@
+/*
+ * File:   main.c
+ * Author: quyenhooppa
+ *
+ * Created on September 21, 2019, 8:56 AM
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
---
+++ Lab2/DigitalWatch.X/system.c
@@ -0,0 +1,26 @@
+#include "system.h"
+
+void oscillationInitialize(void) {
+    OSCCON = 0b01110111; //oscillation 8MHz 
+    OSCTUNE = 0b00001111;
+}
+
+void timerInitialize(void) {
+    //Timer 0
+    INTCONbits.TMR0IE = 1;
+    INTCONbits.GIE = 1;
+    T0CON = 0b11000110;
+    //TMR0L = defineTMR0Register();
+}
+
+void buttonInitialize(void) {
+    TRISAbits.TRISA5 = 1;
+    TRISBbits.TRISB0 = 1;
+    ADCON1 = 0b00001111;
+}
+
+void ledInitialize(void) {
+    TRISD = 0x00;
+    LATD = 0x00;
+}
+
